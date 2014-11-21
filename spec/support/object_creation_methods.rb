c359cc43237f417bbaaa0a44b6adc7c435206980
Kendra <kdodds@gmail.com>
2014-11-21 14:01:42 -0700

    add merge command to feature spec tests

--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -1,12 +1,15 @@
-def create_cat
-  Cat.create!(
+def create_cat(overrides = {})
+  Cat.create!({
   name: 'Garfield'
+}.merge(overrides)
   )
 end
 
-def create_toy(cat)
+
+def create_toy(cat, overrides = {})
   Toy.create!(
   name: 'Lasagna',
   cat_id: cat.id
+}.merge(overrides)
   )
 end
